62a5f91884996dce7340d22ca8df030022c79857
macai <macai@skyrity.com>
2020-01-26 13:58:39 +0800

    修改作者名称为macai@skyrity.com

--- src/main/java/com/skyrity/service/WebService.java
+++ src/main/java/com/skyrity/service/WebService.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * @author ： macai
+ * @author ： macai@skyrity.com
  * @date ：2020/01/05 21:01
  * @description : ${description}
  * @path : com.skyrity.service.WebService
